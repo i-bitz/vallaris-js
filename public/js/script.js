c66eb636d3303b01dacbe8794d8cdaffe1f91ce3
Hari Dahal <haridardar@gmail.com>
2018-12-24 17:23:20 +0700

    Add active class css for link

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,6 @@ $(function() {
     var segment = location.pathname.split("/")[1]
 
     if (segment !== '') {
-        $('#side-nav a.nav-link[href^="/' + segment + '"]').addClass('active')
+        $('#side-nav a.nav-link[href^="' + segment + '"]').addClass('active')
     }
 })
