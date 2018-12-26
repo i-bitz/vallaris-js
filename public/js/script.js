57ea26a02ad855fa970c9daea7683a173eced03b
Hari <haridardar@gmail.com>
2018-12-26 12:04:12 +0000

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 $(function() {
-    var segment = location.pathname.split("/")[1]
+    var segment = location.pathname.split("/")[2]
 
     if (segment !== '') {
         $('#side-nav a.nav-link[href^="' + segment + '"]').addClass('active')
